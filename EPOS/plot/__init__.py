a0c46851a3ecf06285f791dffb8c17282e019a70
Gijs Mulders (Laptop) <mulders@lpl.arizona.edu>
2018-05-04 15:58:34 -0700

    more requirements

--- EPOS/plot/__init__.py
+++ EPOS/plot/__init__.py
@@ -1,3 +1,3 @@
-__all__ = ['survey', 'input','output','model', 'architecture','mcmc','occurrence'] 
+__all__ = ['survey', 'input','output','model','mcmc','occurrence'] 
 import survey, input, output, model, mcmc, occurrence
 #import architecture
